0c0dc15a612ab677306342ef1ca32b4d41559ca2
karlacabanas01 <karlacabanas01@gmail.com>
2023-03-15 11:38:33 -0300

    Middleware fix with NextAuth

--- pages/checkout/middleware.ts
+++ pages/checkout/middleware.ts
@@ -1,25 +1,37 @@
-import { NextResponse, type NextRequest } from 'next/server'
-import * as jose from 'jose';
+import { NextResponse, NextFetchEvent, NextRequest} from 'next/server';
+import {getToken} from 'next-auth/jwt';
  
-export async function middleware(req: NextRequest) {
-  const previousPage = req.nextUrl.pathname;
+export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+  // const previousPage = req.nextUrl.pathname;
  
-  if (previousPage.startsWith('/checkout')) {
-    const token = req.cookies.get('token')?.value || '';
+  // if (previousPage.startsWith('/checkout')) {
+  //   const token = req.cookies.get('token')?.value || '';
  
-    try {
-      await jose.jwtVerify(token, new TextEncoder().encode(process.env.JWT_SECRET_SEED));
-      return NextResponse.next();
-    } catch (error) {
-      return NextResponse.redirect(
-        new URL(`/auth/login?p=${previousPage}`, req.url)
-      );
-    }
+  //   try {
+  //     await jose.jwtVerify(token, new TextEncoder().encode(process.env.JWT_SECRET_SEED));
+  //     return NextResponse.next();
+  //   } catch (error) {
+  //     return NextResponse.redirect(
+  //       new URL(`/auth/login?p=${previousPage}`, req.url)
+  //     );
+  //   }
+  // }
+  const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET }); //Obtenemos la sesion
+
+  console.log({ session });
+
+  if ( !session ) { //Si notenemos ninguna sesion redireccionamos a login
+      const requestedPage = req.page.name;
+      return NextResponse.redirect(`/auth/login?p=${ requestedPage }`);
   }
-};
+
+  return NextResponse.next();
+
+
+}
  
-export const config = {
-  matcher: [
-    '/checkout/:path*'
-  ],
-};
+// export const config = {
+//   matcher: [
+//     '/checkout/:path*'
+//   ],
+// };
